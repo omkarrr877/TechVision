d30c3c4529e65ae564af88148fda651cb0e56dab
omkarrr877 <76659606+omkarrr877@users.noreply.github.com>
2021-08-26 13:26:52 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <>
- <footer className="w-100 bg-light text-center">
+ <footer className="w-100 bg-light text-center mt-5">
      <p>© 2021 OmkarBhone. All Right Reserved | Terms and Conditions</p>
  </footer>
         </>
